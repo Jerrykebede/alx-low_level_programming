d57cee42d9953a30e25ea6686258a83a1945a68e
Kidus Selehi <kidusseleshi19@gmail.com>
2022-09-13 23:03:04 -0700

    task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  *
  * * main -entry point
@@ -7,13 +6,8 @@
  * * Return: Always 0 (sucess)
  *
  * */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
