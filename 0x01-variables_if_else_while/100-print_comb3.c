36b0663d098d032c8b0b4c34602b520892412f49
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-15 06:48:15 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-prints all the numbers of base 16 in lowercase, followed by a new line.
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch++)
+{
+for (n = 40; n <= 57; n++)
+{
+if (n > ch)
+{
+putchar(ch);
+putchar(n);
+if (ch != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+return (0);
+}
+}
