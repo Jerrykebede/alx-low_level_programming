9ddb8954f1430dbd424e33d23d1c988fd94738b5
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-15 02:59:30 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -> assign a random number to the variable n each time it is excuted.
+ * and prints out based on a condition 
+ * return: Always 0(sucess) 
+ */
+int main(void)
+{
+int ch;
+for (ch =48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
