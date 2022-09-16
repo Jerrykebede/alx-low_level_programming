32febe89cbbb9a81714dc358beed9b36ac4cc33e
Kidus Selehi <kidusseleshi19@gmail.com>
2022-09-16 07:23:27 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,27 +6,28 @@
  */
 void print_triangle(int size)
 {
-	int row, columns, hash;
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (columns = 1; columns <= size - row; columns++)
-			{
-				_putchar(' ');
-			}
-			for (hash = 1; hash <= row; hash++)
-			{
-				_putchar('#');
-			}
-			if (row == size)
-				continue;
-			_putchar('\n');
-		}
-		_putchar('\n');
-	}
+int row, columns, hash;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (columns = 1; columns <= size - row; columns++)
+{
+_putchar(' ');
+for (hash = 1; hash <= row; hash++)
+{
+_putchar('#');
+}
+if (row == size)
+continue;
+_putchar('\n');
+}
+_putchar('\n');
+}
+}
 }
