25ebb098ed48f6ad7772cb998237bbda3f1c1aaf
Kidus Selehi <kidusseleshi19@gmail.com>
2022-09-19 07:02:42 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - function to check if c is a letter, lowercase or uppercase
  * @c: is the int that will use for the argument of the function
- * Return: 0 
+ * Return: 0
  */
 int _isalpha(int c)
 {
@@ -13,3 +13,5 @@ return (1);
 else
 return (0);
 }
+
+
