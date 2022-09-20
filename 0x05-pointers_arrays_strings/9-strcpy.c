8c854dc140c66a96f3e51b93cb496e3decc1d9f4
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-20 14:56:57 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,16 @@
 *
 *Return: destination
 */
-
 char *_strcpy(char *dest, char *src)
 {
-int inc = 0;
-
-while(*(src + inc) != '\0')
-{
-*(dest + inc) = *(src + inc);
-inc++;
-}
-*(dest + inc) = '\0';
-
-return (dest);
+	int inc = 0;
+	
+	while(*(src + inc) != '\0')
+	{
+		*(dest + inc) = *(src + inc);
+		inc++;
+	}
+	*(dest + inc) = '\0';
+	
+	return (dest);
 }
