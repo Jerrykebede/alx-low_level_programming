046de935f661d0f6b537474dd83534dc8ca273cb
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-20 15:11:13 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
-*_puts -prints a string
-* @str: string to prin
-* Description: prints a string 
-* On success: returns no error
-*/
+ *_puts - prints a string
+ * @str: string to prin
+ * Description: prints a string
+ * On success: returns no error
+ */
 
 void _puts(char *str)
 {
