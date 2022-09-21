14ec566e0ac9ce0936c29236149d486a1dd5ffed
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-20 19:01:18 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int num = 0;
+
 	do { 
 		if (*s == '-')
 			sign *= -1;
@@ -22,6 +23,6 @@ int _atoi(char *s)
 			break;
 
 	} while (*s++);
-	
+
 	return (num * sign);
 }
