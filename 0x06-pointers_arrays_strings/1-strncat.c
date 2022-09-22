62f799066d84e2e16799f3c4145a790ece9c4572
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-22 08:53:56 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,15 +7,15 @@
  * Return: returns new concatenated string
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	 int index = 0, dest_len =0;
+	int index = 0, dest_len = 0;
 
 	 while (dest[index++])
 		 dest_len++;
 
 	 for (index = 0; src[index] && index < n; index++)
-		 dest[dest_len++] = src[inex];
+		 dest[dest_len++] = src[index];
 
 	 return (dest);
 }
