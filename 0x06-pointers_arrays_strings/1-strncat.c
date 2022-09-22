205f43a929085b13b35029dc6b9b5e84a25e8252
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-22 12:15:21 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,9 @@
 char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
-
-	 while (dest[index++])
-		 dest_len++;
+	
+	while (dest[index++])
+		dest_len++;
 
 	 for (index = 0; src[index] && index < n; index++)
 		 dest[dest_len++] = src[index];
