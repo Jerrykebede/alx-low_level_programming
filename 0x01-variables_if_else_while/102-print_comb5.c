3aed2fdada62225bab7775dfb0425518cb662bb0
Eyerualem Kebede <eyerusalem_ke@yahoo.com>
2022-09-22 15:18:24 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,8 +18,8 @@ int main(void)
 				putchar((num1 / 10) + 48);
 				putchar((num1 % 10) + 48);
 				putchar(' ');
-				putchar((umn2 / 10) + 48);
-				putchar((num1 % 10) + 48);
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
 
 				if (num1 != 98 || num2 != 98)
 				{
