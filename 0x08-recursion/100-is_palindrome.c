00a990eaed2b7445e3953f421ab280e8b4ba2558
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-28 15:21:52 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -36,11 +36,11 @@ int checkp(int i, int lg, char *s)
 	}
 	return (1);
 }
+
 /**
 *is_palindrome - checks if a string is a palindrome
 *@s: is the string
-*
-*Return: return 1 if the string is a palindrome or 0 otherwise 
+* Return: return 1 if the string is a palindrome or 0 otherwise
 **/
 int is_palindrome(char *s)
 {
