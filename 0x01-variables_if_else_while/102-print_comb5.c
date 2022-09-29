7871e6cdb01e7126b1d15a6d6b795e542d9bd467
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-29 01:19:28 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main-prints all the numbers of base 16 in lowercase, followed by a new line.
  * Return: Always 0 (sucess)
  */
 int main(void)
 {
-	int num1 = 0, num2;
-	
-	while (num1 <= 99)
+	int num1;
+	int num2;
 	{
-		num2 = num1;
-		while (num2 <= 99)
+		for (num1 = 0; num1 <= 98; num1++)
 		{
-			if (num2 != num1)
+			for (num2 = num1 + 1; num2 <= 99; num2++)
 			{
-				putchar((num1 / 10) + 48);
-				putchar((num1 % 10) + 48);
+				putchar((num1 / 10) + '0');
+				putchar((num1 % 10) + '0');
 				putchar(' ');
-				putchar((num2 / 10) + 48);
-				putchar((num2 % 10) + 48);
-
-				if (num1 != 98 || num2 != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar((num2 / 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num1 == 98 && num2 == 99)
+					continue;
+				putchar(44);
+				putchar(32);
 			}
-			++num2;
 		}
-		++num1;
+		putchar(10);
 	}
-	putchar('\n');
-	return 0;
+	return (0);
 }
