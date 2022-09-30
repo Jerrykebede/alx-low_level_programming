25877a9593cf032e1892634394e9bb1dd1cf41e4
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-30 16:58:00 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,8 @@
 
 /**
  * _strcat -> this a function strcat
- * @dest : first param
- * @src : second param
+ * @dest: first param
+ * @src: second param
  * Return: a string
  */
 char *_strcat(char *dest, char *src)
