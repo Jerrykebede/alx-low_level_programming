ff26a55671530c9a32db521a5d16657fb127a9f4
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-30 04:17:35 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.n>
+/**
+ * main -> this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i<argc; i++)
+	{
+		printf("argv indx %d is = %s\n", i, argv[i]);
+	}
+	return (0);
+}
