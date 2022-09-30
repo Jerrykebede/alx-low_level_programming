bcbfdfbb56737d1558b7296b8e5b0c7f850d9e47
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-30 06:59:58 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -> this is a function to print its name 
+ * main - > this is a function to print its name 
  * @argc: argc parameter
  * @argv: an array of a command listed
  * Return: 0 for success
