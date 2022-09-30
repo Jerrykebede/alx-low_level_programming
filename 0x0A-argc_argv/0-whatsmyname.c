374268d0f5907fc494b7352cd745309d94563968
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-30 02:01:14 -0700

    solved the unused warning

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -> this is a function to print its name 
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: 0 for success
+ *main -> this is a function to print its name 
+ *@argc: argc parameter
+ *@argv: an array of a command listed
+ *Return: 0 for success
  */
-int main(int argc _attribute_((unsed)), char *argv[])
+int main (int argc attribute ((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
