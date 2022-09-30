576549f8e42ca6787019f7b14c4d94744fc1f18b
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-09-30 15:10:28 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,24 +4,35 @@
 * _atoi - Converts a string to an integer.
 *@s: The string to be converted.
 *
-*Return: The integer value of the converted string.
+*Return: a integer
 */
-
-int _atoi(char *s)
+int  _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
-	
-	do{	
-		if (*s == '-')
-			sign *= -1;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
 			break;
+		}
 
-	while} (*s++);
-	return (num * sign);
+		c++;
+	}
+
+	ni *= min;
+	return (ni);
 }
