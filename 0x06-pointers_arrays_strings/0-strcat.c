4015b82cd16bf7384ed92922c2eff16b9c0bb8c5
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-01 06:52:45 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcat - concatentes two strings
+ * _strcat - concatenates two strings.
  * @dest: The destination string
  * @src: The source string
  *
