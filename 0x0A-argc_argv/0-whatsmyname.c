03abdb392ed893b2037471a8dbbee308e5dcc250
kidussele <kidusseleshi19@gmail.com>
2022-09-30 21:19:05 -0700

    All task is Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -> this a function strcat
+ * @argc : first parameter
+ * @argv : second parameter
+ * Return: 0 success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+		printf("%s\n", *argv);
+			return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -> this a function strcat
+ * @argc : first parameter
+ * @argv : second parameter
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
