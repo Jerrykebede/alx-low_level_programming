184cfe08bb3bcae6fe63966bd131af2f7afe7140
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-03 03:26:34 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,7 +6,7 @@
  *
  * Return: No. of words in the string (int)
  */
-char **strtow(char *str)
+char **wodnos(char *s)
 {
 	int flag, c, w;
 
@@ -24,4 +24,51 @@ char **strtow(char *str)
 		}
 	}
 	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split 
+ *
+ * Return: pointer to an array of strings (success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = wordnos(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c =0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
