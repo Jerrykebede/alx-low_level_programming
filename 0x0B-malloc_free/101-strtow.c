49f7d8d78cf72fa6401b93e5c41de809a858cc6a
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-03 02:55:20 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * wordnos - counts no of words in a given string
+ * @s: pointer to the string
+ *
+ * Return: No. of words in the string (int)
+ */
+char **strtow(char *str)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);
+}
