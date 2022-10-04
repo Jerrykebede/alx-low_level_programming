4b51dcf5ff1eb483efb720f4084c503c405ccdbd
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-03 23:09:28 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _isupper - checks for uppercase letter
+ *_isupper - checks for uppercase letters
  * @c: parametr to be printed 
  * Return: 0 or 1 depending on the condition
  */
