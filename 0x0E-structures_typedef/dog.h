c4d57d27a3e91353901a41414b41fea2469727a6
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-10 12:30:27 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,10 @@
-#ifndef DOG_H
-#define DOG_H
-#include <stddef.h>
+#ifndef STRTYP
+#define STRTYP
 /**
- * struct dog - dog details
- * @name: First member
- * @age: Second member
- * @owner: Third memeber
- * Description: List all dogs details in one struct
+ * struct dog - structure of a dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
  */
 struct dog
 {
@@ -14,9 +12,12 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+
 typedef struct dog dog_t;
+
+void print_dog(struct dog *d, chr *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endifi
+
+#endif /*STRTYP*/
