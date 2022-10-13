c600d4f899fb7422b80645f0ad3702b01549b843
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-13 12:57:35 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (array != NULL && action != NULL && size > 0)
+	if (array && size > 0 && action)
 	{
-		while (i < size)
+		for (i = 0; i < size: i++)
 		{
 			action(array[i]);
-			i++;
 		}
 	}
 }
