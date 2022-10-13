62b25045f3280075111f0d8f02a98798c32dccb6
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-13 11:15:33 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		separator = "";
+
 	va_start(strings, n);
 
 	for (i = 0; i < n; i++)
@@ -27,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", s);
 		if (i < n - 1)
 		{
-			printf("%s", separaor);
+			printf("%s", separator);
 		}
 	}
 	Printf("\n")
