3edee33da095dfb558b774d59e9a2804d06f3d46
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-25 13:01:07 -0700

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * sum_ listint - Calculates the sum of all the
  * data (n) of a listint_t list.
@@ -13,7 +14,7 @@ int sum_listint(listint_t *head)
 
 	while (head)
 	{
-		sum += head->n;
+		sum + = head->n;
 		head = head->next;
 	}
 	return (sum);
