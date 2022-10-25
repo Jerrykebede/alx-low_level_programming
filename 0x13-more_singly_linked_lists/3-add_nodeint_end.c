2f928f73bc9e6e48103ec209589a537b5396921b
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-25 03:55:46 -0700

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,17 +13,14 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *last;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	
 	new->n = n;
 	new->next = NULL;
-	
 	if (*head == NULL)
 		*head = new;
-	
 	else
 	{
 		last = *head;
@@ -31,6 +28,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			last = last->next;
 		last->next = new;
 	}
-	
 	return (*head);
 }
