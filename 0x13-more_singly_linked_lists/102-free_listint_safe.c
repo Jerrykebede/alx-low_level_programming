1582f873f2c2a4dba3c88653871c7c1af444c885
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-25 14:57:00 -0700

    task 3

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -45,7 +45,7 @@ size_t looped_listint_count(listint_t *head)
 
 /*
  * free_listint_safe - Frees a listint_t list safely (ie.
- * can free lists containing loops)
+ * canfree lists containing loops)
  * @h: A pointer to the address of
  * the head of the listint_t list.
  *
