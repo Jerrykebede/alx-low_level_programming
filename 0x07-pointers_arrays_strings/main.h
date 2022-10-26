f2730d1cf3c56f3573fbfd8ad2b211bf6af804f1
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-10-26 07:28:30 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char C);
 
@@ -17,7 +17,7 @@ char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
 
-void set_string(char **s, char *to); 
+void set_string(char **s, char *to);
 
 void print_diagsums(int *a, int size);
 
