10125f447b4b51291b9979a53bc7ab8b4e9ba087
Eyerusalem Kebede <eyerusalem_ke@yahoo.com>
2022-11-08 12:02:24 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
-
-
 int main(void)
-
 {
 
-	int n;
-	
+	int n;	
 	int a[5];
-	
 	int *p;
-
-	
 	
 	a[2] = 1024;
-	
 	p = &n;
-
 /**
  * write your line of code here...
  * Remember:
@@ -26,7 +17,7 @@ int main(void)
  *   - only one statement
  *   - you are not allowed to code anything else than this line of code
  */
-	p[5] = 98;
+	*(p + 5) = 98;
 
 /* ...so that this prints 98\n */
 	
