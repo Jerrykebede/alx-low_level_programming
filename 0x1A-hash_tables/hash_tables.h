f1d4dbcd366a4badcafc7c2e62525b63ea845693
JerryKebede <eyerusalem_ke@yahoo.com>
2024-06-07 11:57:37 -0700

    all

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -13,34 +13,34 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-typedef struct hash_node_s
+typedef struct sash_node_s
 {
 	char *key;
 	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
+	struct sash_node_s *next;
+} sash_node_t;
 
 /**
- * struct hash_table_s - Hash table data structure
+ * struct sash_table_s - Sash table data structure
  *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-typedef struct hash_table_s
+typedef struct sash_table_s
 {
 	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
+	sash_node_t **array;
+} sash_table_t;
 
-sash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
+sash_table_t *sash_table_create(unsigned long int size);
+unsigned long int sash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
+int sash_table_set(sash_table_t *ht, const char *key, const char *value);
+char *sash_table_get(const sash_table_t *ht, const char *key);
+void sash_table_print(const sash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
+void sash_table_delete(sash_table_t *ht);
 
 #endif
